8f1646c6805c75a6fa11227e573432adc7660b41
ArsArifurRahman <ars.ict.developer@gmail.com>
2024-03-17 13:05:35 +0600

    Modified Database Context

--- Project/Persistence/DataContext.cs
+++ Project/Persistence/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
@@ -7,5 +8,22 @@ public class DataContext : IdentityDbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
+    }
+
+    public DbSet<ProductCatalogType> ProductCatalogTypes { get; set; } // Corrected the property name
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure the ProductCatalogType entity
+        modelBuilder.Entity<ProductCatalogType>(entity =>
+        {
+            entity.HasMany(e => e.ProductCatalog)
+            .WithOne(e => e.CatalogType)
+            .HasForeignKey(e => e.CatalogTypeId);
+        });
+
+        modelBuilder.Entity<Account>().HasOne(a => a.Institution)
+        .WithOne(i => i.Account)
+        .HasForeignKey<Institution>(i => i.DefaultAccountId);
     }
 }
